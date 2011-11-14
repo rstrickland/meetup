27fbe8a1c4ed35946bebb0250657afd8007377e7
Robbie Strickland <rostrickland@gmail.com>
2011-11-14 13:19:08 -0500

    Fixing sbt project file to include dependencies

--- camel/src/main/scala/CamelActor.scala
+++ camel/src/main/scala/CamelActor.scala
@@ -3,28 +3,6 @@ import akka.actor.Actor._
 import akka.camel.{Message, Consumer, Producer, Failure, Oneway}
 import akka.camel.{CamelServiceManager, CamelContextManager}
 
-class CamelConsumer extends Actor with Consumer {
-  def endpointUri = "vm:test"
-  //def endpointUri = "mina:tcp://localhost:6200"
-  //def endpointUri = "jetty:http://localhost:6200/test"
-  
-
-  def receive = {
-    case msg: Message => 
-      val msgStr = msg.getBodyAs(classOf[String])
-      println(msgStr)
-
-      //self.reply("ACK")
-  }
-}
-
-class CamelProducer extends Actor with Producer with Oneway {
-  def endpointUri = "vm:test"
-  //def endpointUri = "mina:tcp://localhost:6200"
-  //def endpointUri = "jetty:http://localhost:6200/test"
-  
-}
-
 object CamelTest extends App {
   val service = CamelServiceManager.startCamelService
   val prod = actorOf[CamelProducer].start
@@ -39,9 +17,30 @@ object CamelTest extends App {
   val time = System.currentTimeMillis - start
   println("Message send time: " + time + " ms")
 
-  Thread.sleep(60000)  
+  Thread.sleep(5000)  
   
   sys.exit
+}
+
+class CamelConsumer extends Actor with Consumer {
+  //def endpointUri = "vm:test" // use SEDA component
+  //def endpointUri = "mina:tcp://localhost:6200" // use TCP over MINA
+  def endpointUri = "jetty:http://localhost:6200/test" // use HTTP over jetty
+  
+
+  def receive = {
+    case msg: Message => 
+      val msgStr = msg.getBodyAs(classOf[String])
+      println(msgStr)
+
+      self.reply("ACK")
+  }
+}
+
+class CamelProducer extends Actor with Producer with Oneway {
+  //def endpointUri = "vm:test"
+  //def endpointUri = "mina:tcp://localhost:6200"
+  def endpointUri = "jetty:http://localhost:6200/test"
 }
 
 
